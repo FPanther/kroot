a826de1198f76f6581cc9529608497863ddff616
Fpan020 <40954356+Fpan020@users.noreply.github.com>
2020-03-22 21:35:43 +1300

    made another command line argument the path of py

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,26 @@
 // command line arguments: 
+//NAME: orth_to_surprisals
+// SUMMARY: script for running python functions successively 
 // args[0] - this file
-// args[1] - python compiler
-// args[2] - folder which contains kroot.csv
+// args[1] - python compiler, e.g. "~\\Python\\Python38-32\\python.exe"
+// args[2] - folder which contains kroot.csv e.g. "~\\Chapters\\Minimal_Root\\tables"
+// args[3] - folder which contains py scripts kComp, kEntropy, 
+//              kSurprisal lex_io, orth_to_ipa.py, produce_segmental_configurations_list.py.
+//           e.g. "~\\src\\py"
 use std::env;
 use std::process;
 
 fn main() {
    let args: Vec<String> = env::args().collect();
    // confirm that there are two command line argument
-    if args.len() != 3 {
+    if args.len() != 4 {
         panic!("Wrong number of command line arguments!")
     }
     //set relevant strings
 
-    let orth_to_ipa_dir = env!("CARGO_MANIFEST_DIR").to_string() + "\\src\\py\\orth_to_ipa.py";
-    let kcomp_dir = env!("CARGO_MANIFEST_DIR").to_string() + "\\src\\py\\kComp.py";
-    let seg_config_dir = env!("CARGO_MANIFEST_DIR").to_string() + "\\src\\py\\produce_segmental_configurations_list.py";
+    let orth_to_ipa_dir = args[3].to_string() + "\\orth_to_ipa.py";
+    let kcomp_dir = args[3].to_string() + "\\kComp.py";
+    let seg_config_dir = args[3].to_string() + "\\produce_segmental_configurations_list.py";
 
     //orth to ipa procedure
     let kroot_path = args[2].to_string() + "\\kroot.csv";
